4b045960335e154a27e625380419ccaf44b0c3d7
jamirali <jamirali720@gmail.com>
2024-05-23 15:10:59 +0600

    changed

--- src/app/configs/index.ts
+++ src/app/configs/index.ts
@@ -1,11 +1,9 @@
 import dotenv from "dotenv";
+import path from "path"
+dotenv.config({path: path.join(process.cwd(), ".env") });
 
-dotenv.config();
 
 export default {
   port: process.env.PORT,
-  databaseUrl:
-    process.env.NODE_ENV === "development"
-      ? process.env.MONGODB_LOCAL_DATABASE_URL
-      : process.env.MONGODB_PRODUCTION_DATABASE_UR,
+  databaseUrl: process.env.MONGODB_PRODUCTION_DATABASE_URL,
 };
